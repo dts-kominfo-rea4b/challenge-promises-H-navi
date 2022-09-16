deea75d036ffc300c69f60bf9332c1b4203a18c7
H-navi <hanafinaufal@protonmail.com>
2022-09-16 21:07:37 +0700

    tugas promise

--- promise.js
+++ promise.js
@@ -1,8 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+    const ixx = await promiseTheaterIXX();
+    const vgc = await promiseTheaterVGC();
+    const mergedData = [...ixx, ...vgc];
+
+    let jumlahEmosi = 0;
+    ixx.forEach(hitung);
+    vgc.forEach(hitung);
+
+    function hitung(item, index) {
+        if (item.hasil == emosi) { jumlahEmosi++ }
+    }
+    return jumlahEmosi;
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
